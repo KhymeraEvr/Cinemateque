6f092e8a84e0c71fefdab9029946920d78b187e1
Bohdan Khymera <bogdankhymera@gmail.com>
2019-04-21 13:32:44 +0300

    Add project files.

---
+++ Cinemateque/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cinemateque.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
